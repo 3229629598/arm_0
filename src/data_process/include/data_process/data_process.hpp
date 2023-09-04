60001cee082d627344644409ca08e8e8cd1ec265
HY_Lin <3229629598@qq.com>
2023-09-04 20:09:10 +0800

    arm_0

---
+++ src/data_process/include/data_process/data_process.hpp
@@ -0,0 +1,41 @@
+#ifndef data_process_hpp
+#define data_process_hpp
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/u_int8_multi_array.hpp>
+#include <std_msgs/msg/u_int8.hpp>
+#include <trajectory_msgs/msg/joint_trajectory_point.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <data_process/crc.hpp>
+#include <data_process/packet.hpp>
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include <chrono>
+
+namespace data_process_ns
+{
+    class Data_Process : public rclcpp::Node
+    {
+        public:
+        Data_Process(const rclcpp::NodeOptions & options);
+        ~Data_Process();
+        private:
+        rclcpp::CallbackGroup::SharedPtr callback_group_sub;
+        rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr rx_sub;
+        void process_callback(const std_msgs::msg::UInt8MultiArray::SharedPtr rx_buf);
+        rclcpp::Publisher<std_msgs::msg::UInt8MultiArray>::SharedPtr tx_pub;
+        std_msgs::msg::UInt8MultiArray tx_buf;
+        rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr arm_request_pub;
+        std_msgs::msg::UInt8 arm_request;
+        rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_pub;
+        sensor_msgs::msg::JointState joint_state;
+        rclcpp::Subscription<trajectory_msgs::msg::JointTrajectoryPoint>::SharedPtr jtp_sub;
+        void jtp_callback(const trajectory_msgs::msg::JointTrajectoryPoint::SharedPtr jtp_data);
+        uint8_t joint_num;
+    };
+}
+
+#endif
---
+++ src/data_process/include/data_process/packet.hpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2022 ChenJun
+// Licensed under the Apache-2.0 License.
+
+#ifndef packet_hpp
+#define packet_hpp
+
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
+namespace data_process_ns
+{
+struct rx_bag
+{
+  uint8_t header = 0x5A;
+  float joint_position[6];
+  uint8_t arm_request;
+  uint16_t checksum;
+} __attribute__((packed));
+
+struct tx_bag
+{
+  uint8_t header = 0xA5;
+  float joint_goal[6];
+  uint16_t checksum;
+} __attribute__((packed));
+
+#define tx_len sizeof(tx_bag)
+#define rx_len sizeof(rx_bag)
+
+inline rx_bag fromVector(const std::vector<uint8_t> & data)
+{
+  rx_bag packet;
+  std::copy(data.begin(), data.end(), reinterpret_cast<uint8_t *>(&packet));
+  return packet;
+}
+
+inline std::vector<uint8_t> toVector(const tx_bag & data)
+{
+  std::vector<uint8_t> packet(tx_len);
+  std::copy(
+    reinterpret_cast<const uint8_t *>(&data),
+    reinterpret_cast<const uint8_t *>(&data) + tx_len, packet.begin());
+  return packet;
+}
+
+}
+
+#endif
