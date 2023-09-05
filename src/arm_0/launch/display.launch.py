51f4fca447945aa72651441fbb63038c541f0e43
HY_Lin <3229629598@qq.com>
2023-09-05 17:36:28 +0800

    arm_0

--- src/arm_0/launch/display.launch.py
+++ src/arm_0/launch/display.launch.py
@@ -4,37 +4,38 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     package_name = 'arm_0'
     urdf_name = "arm_0.urdf"
-
+    rviz_name = "urdf.rviz"
+    
     ld = LaunchDescription()
-    pkg_share = FindPackageShare(package=package_name).find(package_name)
+    pkg_share=get_package_share_directory(package_name)
     urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    rviz_config_path=os.path.join(pkg_share,f'rviz/{rviz_name}')
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         arguments=[urdf_model_path]
         )
 
-    fake_joint_pub=LaunchConfiguration("fake_test", default="true")
     joint_state_publisher_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         arguments=[urdf_model_path],
-        condition=IfCondition(fake_joint_pub)
         )
 
-    launch_rviz=LaunchConfiguration("display_module", default="true")
     rviz2_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
+        arguments=['-d',rviz_config_path],
         output='screen',
-        condition=IfCondition(launch_rviz)
         )
 
     ld.add_action(joint_state_publisher_node)
