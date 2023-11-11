3997043e018195c97806ca323670127548be7cd3
HY_Lin <3229629598@qq.com>
2023-11-11 14:58:48 +0800

    0

--- launch.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-gnome-terminal -- bash -c "cd arm_0;source install/setup.bash;ros2 launch bringup bringup.launch.py"
--- src/serial_py/serial_py/serial_config.py
+++
@@ -1,6 +0,0 @@
-port_name: str = "/dev/ttyACM_c"
-port_baudrate = 115200
-port_bytesize = 8
-port_stopbits = 1
-port_parity: str = "N"
-port_timeout = 0.5
